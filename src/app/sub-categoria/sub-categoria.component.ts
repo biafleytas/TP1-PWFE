ad81bbaa7dfd005564b80f3dc3da0dc5f53dc3a1
Sonia Fleytas <beatrizfleytas@gmail.com>
2021-09-08 20:58:38 -0400

    missing files

---
+++ src/app/sub-categoria/sub-categoria.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {SubCategoria} from "../model/sub-categoria";
+import {ServicecategoriaService} from "../service/servicecategoria.service";
+import {ServicesubcategoriaService} from "../service/servicesubcategoria.service";
+
+@Component({
+  selector: 'app-sub-categoria',
+  templateUrl: './sub-categoria.component.html',
+  styleUrls: ['./sub-categoria.component.css']
+})
+export class SubCategoriaComponent implements OnInit {
+  subCategorias: SubCategoria[] = [];
+
+  constructor(private servicioSubCategoria: ServicesubcategoriaService) { }
+
+  ngOnInit(): void {
+    this.servicioSubCategoria.getSubCategorias().subscribe(
+      entity => this.subCategorias = entity.lista,
+      error =>console.log('no se pudieron conseguir las sub-categorias')
+    );
+
+  }
+
+  deleteItem(p: number){
+    this.servicioSubCategoria.deleteSubCategoria(p)
+      .subscribe(response => {
+        this.subCategorias = this.subCategorias.filter(item => item.idTipoProducto !== p);
+      })
+  }
+
+}
