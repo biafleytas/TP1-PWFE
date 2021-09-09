420baa6b8a57849c7d6f9c5a2fdc6f7957f8c0fd
Sonia Fleytas <beatrizfleytas@gmail.com>
2021-09-08 20:57:37 -0400

    missing files

---
+++ src/app/service/servicesubcategoria.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {listadatos} from "../model/datos";
+import {Categoria} from "../model/categoria";
+import {SubCategoria} from "../model/sub-categoria";
+import {tap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesubcategoriaService {
+
+  private api: string ="http://181.123.243.5:8080/stock-pwfe/tipoProducto";
+
+  constructor(private http: HttpClient) { }
+
+  getSubCategorias(): Observable<listadatos<SubCategoria>> {
+    return this.http.get<listadatos<SubCategoria>>(this.api);
+  }
+
+  agregarSubCategoria(p:SubCategoria): Observable<SubCategoria> {
+    return this.http
+      .post<SubCategoria>(this.api, p)
+      .pipe(
+        tap( // Log the result or error
+
+          data => console.log('agregado '+data),
+          error => console.log("error: "+error)
+        )
+      );
+  }
+
+  deleteSubCategoria(id: number){
+    return this.http.delete(this.api+'/'+id);
+  }
+}
